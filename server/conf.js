ec4bee29cdf4e4272ec800f6d2e56a32dde68b96
yearnlight <291190413@qq.com>
2021-01-28 11:27:38 +0800

    forS

---
+++ server/conf.js
@@ -0,0 +1,8 @@
+//数据库配置
+const conf = {
+    "dbUser": "xuanwo",
+    "dbPassword": "Q1qaz@WSX",
+
+}
+
+module.exports = conf;
--- server/db.config.js
+++ server/db.config.js
@@ -1,10 +1,12 @@
+//导入配置文件
+const base_conf = require("./conf");
 //数据库配置
 const conf = {
-    "host":"81.68.200.164",
-    "user":"xuanwo",
-    "password":"Xuan2@wo",
-    "database":"task",
-    "port":"3306"
+    "host": "81.68.200.164",
+    "user": base_conf.dbUser,
+    "password": base_conf.dbPassword,
+    "database": "task",
+    "port": "3306"
 }
 
 module.exports = conf;
